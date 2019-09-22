a6300861cb901d9d2b1367074592784c9520e8fe
Anthony Bernard <tony@anthonys-mbp.home>
2019-09-22 11:35:13 -0400

    minor changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,14 @@ export default function Header() {
     return (
         <div className="container text-center mt-3">
             <h2>Deals</h2>
-            <p>This is a Discount app written in React JS. It uses the API from Discountsapi.com for the data.</p>
+            <p>This is a Discount app written in React JS. It uses the API from Discountsapi.com for the data. See code for this project
+            <a
+              href="https://github.com/apb305/react-discount-app"
+              target="noopener noreferrer"
+            >
+              {" "}
+              here
+            </a></p>
             <p className="small">Created By: Anthony Bernard</p>
         </div>
     )
